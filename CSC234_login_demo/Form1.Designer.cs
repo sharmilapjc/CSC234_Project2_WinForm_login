e83d0d0d0ab8191aa73de82d3ea35456eb884e81
palaniss <palaniss@ecs.csus.edu>
2019-04-08 19:34:03 -0700

    Initial commit

---
+++ CSC234_login_demo/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace CSC234_login_demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtbox_username = new System.Windows.Forms.TextBox();
+            this.txtbox_password = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label_invisible = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(212, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(362, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CSC 234 DEMO LOGIN";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(275, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(191, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter Username";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(275, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(189, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Enter Password";
+            // 
+            // txtbox_username
+            // 
+            this.txtbox_username.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbox_username.Location = new System.Drawing.Point(504, 114);
+            this.txtbox_username.Name = "txtbox_username";
+            this.txtbox_username.Size = new System.Drawing.Size(253, 28);
+            this.txtbox_username.TabIndex = 3;
+            // 
+            // txtbox_password
+            // 
+            this.txtbox_password.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbox_password.Location = new System.Drawing.Point(504, 199);
+            this.txtbox_password.Name = "txtbox_password";
+            this.txtbox_password.PasswordChar = '*';
+            this.txtbox_password.Size = new System.Drawing.Size(253, 28);
+            this.txtbox_password.TabIndex = 4;
+            // 
+            // btn_login
+            // 
+            this.btn_login.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_login.Location = new System.Drawing.Point(305, 305);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(137, 48);
+            this.btn_login.TabIndex = 5;
+            this.btn_login.Text = "LOGIN";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_cancel.Location = new System.Drawing.Point(504, 305);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(137, 48);
+            this.btn_cancel.TabIndex = 6;
+            this.btn_cancel.Text = "CANCEL";
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            this.btn_cancel.Click += new System.EventHandler(this.btn_cancel_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(36, 111);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(203, 184);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label_invisible
+            // 
+            this.label_invisible.AutoSize = true;
+            this.label_invisible.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_invisible.Location = new System.Drawing.Point(205, 386);
+            this.label_invisible.Name = "label_invisible";
+            this.label_invisible.Size = new System.Drawing.Size(0, 25);
+            this.label_invisible.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 450);
+            this.Controls.Add(this.label_invisible);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.txtbox_password);
+            this.Controls.Add(this.txtbox_username);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtbox_username;
+        private System.Windows.Forms.TextBox txtbox_password;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Button btn_cancel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label_invisible;
+    }
+}
+
---
+++ CSC234_login_demo/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace CSC234_login_demo
+{
+    public partial class Form1 : Form
+    {
+        static int numAttempts = 2;
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = new Bitmap(@"..\..\images\login.png");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            if (numAttempts == 0)
+            {
+                label_invisible.Text = ("MAXIMUM ATTEMPTS REACHED. ACCESS DENIED!");
+                return;
+            }
+
+            SqlConnection scn = new SqlConnection();
+            scn.ConnectionString = @"Data Source=LAPTOP-A2QS35FN\SQLEXPRESS;Initial Catalog=credentials;database=CSC_234_LOGIN;integrated security=SSPI";
+            SqlCommand scmd = new SqlCommand("select count (*) as cnt from credentials where username=@usr and password=@pwd", scn);
+            scmd.Parameters.Clear();
+            scmd.Parameters.AddWithValue("@usr", txtbox_username.Text);
+            scmd.Parameters.AddWithValue("@pwd", txtbox_password.Text);
+            scn.Open();
+
+            if (scmd.ExecuteScalar().ToString() == "1")
+            {
+                pictureBox1.Image = new Bitmap(@"..\..\images\granted.jpg");
+                label_invisible.Text = "";
+                MessageBox.Show("ACCESS IS GRANTED!");
+            }
+            else
+            {
+                pictureBox1.Image = new Bitmap(@"..\..\images\denied.jpg");
+                MessageBox.Show("ACCESS IS DENIED!");
+                label_invisible.Text = (Convert.ToString(numAttempts) + " Attempts Left");
+                --numAttempts;
+                txtbox_username.Clear();
+                txtbox_password.Clear();
+            }
+            scn.Close();
+        }
+
+        private void btn_cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
